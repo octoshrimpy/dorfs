a7b36dc5ac9cf3d2b67d792dbf60e32cea4b23c6
Rocco Nicholls <rocco11nicholls@gmail.com>
2022-05-01 21:22:32 -0600

    Extracted a bunch of common functions and did some refactoring. Also sprite walking is speed based now

--- alives/mobs/cow.js
+++ alives/mobs/cow.js
@@ -13,46 +13,14 @@ export default class Cow extends BaseMob {
     this.walk_speed = rand(5, 15) // 0-100
 
     Cow.objs.push(this)
-    this.changeDest()
-  }
-
-  changeDest() {
-    this.destination = {
-      x: rand(32, this.ctx.game.config.width - 32),
-      y: rand(32, this.ctx.game.config.height - 32),
-    }
-  }
-
-  walkTowardsDest() {
-    // https://phaser.io/news/2018/03/pathfinding-and-phaser-3
-    if (!this.destination) { return }
-
-    var dx = this.destination.x - this.sprite.x
-    var dy = this.destination.y - this.sprite.y
-    if (Math.abs(dx) < 5) { dx = 0 }
-    if (Math.abs(dy) < 5) { dy = 0 }
-
-    if (dx == 0 && dy == 0) {
-      this.destination = undefined
-      this.sprite.anims.stop(null)
-      return
-    }
-
-    // this.sprite.anims.play([this.sprite, "walk"].join("."), true)
-    this.sprite.flipX = dx < 0
-    var max_speed = 2, max_speed_scale = 100
-    var scaled_speed = (this.walk_speed / max_speed_scale) * max_speed
-    var speed_scale = scaled_speed / (Math.abs(dx) + Math.abs(dy))
-
-    this.sprite.x += dx * speed_scale
-    this.sprite.y += dy * speed_scale
+    this.setRandomDest()
   }
 
   tick() {
     if (randOnePerNSec(10) == 0) {
-      this.changeDest()
+      this.setRandomDest()
     }
 
-    this.walkTowardsDest()
+    this.moveTowardsDest()
   }
 }
--- baseClass.js
+++ baseClass.js
@@ -5,14 +5,28 @@ export default class BaseClass {
     this.opts = opts || {}
 
     this.sprite_path = sprite_path || "placeholder"
-    this.sprite = this.setSprite.call(this, this.sprite_path)
+    this.sprite = this.setSprite(this.sprite_path)
   }
 
   setSprite(spriteStr) {
     let sprite = this.ctx.addSpriteAnim(this.opts.x, this.opts.y, spriteStr).setDepth(1)
     sprite.name = spriteStr
+    this.sprite_anims = Object.keys(sprite.anims.animationManager.anims.entries)
 
     return sprite
+  }
+
+  spriteHasAnim(anim) {
+    return this.sprite_anims.includes([this.sprite_path, anim].join("."))
+  }
+
+  loopAnim(anim) {
+    let continue_anim_if_already_playing = true
+    this.sprite.anims.play([this.sprite_path, anim].join("."), continue_anim_if_already_playing)
+  }
+
+  stopAnim() {
+    this.sprite.anims.stop(null)
   }
 
   // Class method
