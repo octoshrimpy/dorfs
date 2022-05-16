6c190e27330864da7e19db0ba20373ed999a5698
Rocco Nicholls <rocco11nicholls@gmail.com>
2022-05-15 22:55:01 -0600

    Added descriptions to all helpers

--- helpers.js
+++ helpers.js
@@ -3,13 +3,18 @@ var fps = 60
 export function scaleX(n) { return n * 16 }
 export function scaleY(n) { return n * 16 }
 
+// Given a list of [item, weight] returns a flat list of the items multiplied by their weight as a large list
+// [["this", 2], ["that", 1]]
+// Becomes: ["this", "this", "that"]
 export function weightedList() {
   return Array.from(arguments).map(function(arg) {
-    return byWeight(arg[0], arg[1])
+    return times(arg[1], function() { return arg[0] })
   }).flat()
 }
 
-// multiplier is the multiple of how much more common the center is than the outsides
+// Returns an integer between min and max, inclusive.
+// Follows a bell curve- center numbers are `multiplier` times more likely to appear than outsides
+// If provided, `bias` will act as the top of the bell curve
 export function normalDist(min, max, multiplier=3, bias=null) {
   bias = bias || ((max - min) / 2)
   let weighted_values = times(multiplier, function() { return rand(min, max) })
@@ -19,29 +24,34 @@ export function normalDist(min, max, multiplier=3, bias=null) {
   return Math.round((norm * (1 - mix)) + (bias * mix))
 }
 
-export function times(times, fn) {
-  return Array(times).fill().map(function(_, idx) {
+// Runs the `fn` function `count` times, optionally passing the 0-based index as the argument.
+// Will return the `return` of each function as an array.
+export function times(count, fn) {
+  return Array(count).fill().map(function(_, idx) {
     return fn(idx)
   })
 }
 
-export function byWeight(obj, weight) {
-  return times(weight, function() { return obj })
-}
-
+// Given an x and y for a sprite sheet, returns the index of the sprite.
 export function idxFromPos(x, y, sheet_cells_horz = 32) {
   // 0 based pos
   return (y * sheet_cells_horz) + x
 }
 
+// Add all integers in an array together
 export function sum(arr) {
   return arr.reduce(function(aggr, i) { return aggr + i }, 0)
 }
 
+// Returns a random value from an array
 export function sample(arr) {
-  return arr[Math.floor(rand() * arr.length)]
+  return arr[rand(arr.length)]
 }
 
+// Returns a random value.
+// If no args are given, returns a random float between 0,1
+// If one arg is given, returns an integer between 0,arg inclusive
+// If two args are given, returns an integer between min,max inclusive
 export function rand(min, max) {
   if (!min) { return Math.random() }
   if (!max) {
@@ -54,14 +64,20 @@ export function rand(min, max) {
   return Math.floor(rand() * (max - min) + min)
 }
 
+// Will return `true` approximately once every `n` seconds when called every tick
 export function randOnePerNSec(n) {
-  return rand(fps * n)
+  return rand(fps * n) == 0
 }
 
+// Will return `true` approximately `n` times every second when called every tick
 export function randNPerSec(n) {
-  return rand(fps / n)
+  return rand(fps / n) == 0
 }
 
+// Transforms `value` through a scale.
+// `value` is expected to be within f1,f2, and based on it's percentage through those, will return
+//   the equivalent between t1,t2
+// `value` can be used outside of the given ranges and will be scaled appropriately
 export function scaleVal(value, f1, f2, t1, t2) {
   var tr = t2 - t1
   var fr = f2 - f1
